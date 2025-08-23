e58ff6c231c716c044956011eb31452bccd31249
Domagoj <paulic.domagoj@gmail.com>
2025-08-23 12:43:29 +0200

    implement sequential chain for code and test generation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from langchain_openai import ChatOpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
+from langchain.chains import LLMChain, SequentialChain
 from dotenv import load_dotenv
 import argparse
 
@@ -16,18 +16,40 @@ llm = ChatOpenAI(
 )
 
 code_prompt = PromptTemplate(
+    input_variables=["language", "task"],
     template="Write a very short {language} function that will {task}",
-    input_variables=["language", "task"]
+)
+
+test_prompt = PromptTemplate(
+    input_variables=["language", "code"],
+    template="Write a test for the following {language} code:\n{code}"
 )
 
 code_chain = LLMChain(
     llm=llm,
     prompt=code_prompt,
+    output_key="code"
 )
 
-result = code_chain({
+test_chain = LLMChain(
+    llm=llm,
+    prompt=test_prompt,
+    output_key="test"
+)
+
+chain = SequentialChain(
+    chains=[code_chain, test_chain],
+    input_variables=["language", "task"],
+    output_variables=["code", "test"]
+)
+
+result = chain({
     "language": args.language,
     "task": args.task
 })
 
-print(result["text"])
+print("----- Generated Code -----\n")
+print(result["code"])
+
+print("\n----- Generated Test -----\n")
+print(result["test"])
